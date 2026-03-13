0cbc4badfcd31f6f672750f7377197286759cda9
Madhu Sudhan <2300031210@kluniversity.in>
2026-03-13 23:51:18 +0530

    Zooming Logic added

--- Open CV/AirZoom.py
+++ Open CV/AirZoom.py
@@ -1,9 +1,9 @@
 import cv2
 import numpy as np
 import mediapipe as mp
+import time
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
-import time
 
 MODEL_PATH = r"D:\Machine Learning\Open CV\hand_landmarker.task"
 
@@ -23,38 +23,56 @@ smoothed_landmarks = None
 prev_time = 0
 prev_distance_smooth = None
 DIST_ALPHA = 0.3
+DELTA_THRESHOLD = 0.0015
 
-capture = cv2.VideoCapture(0)
+last_zoom_time = 0
+ZOOM_COOLDOWN = 0.25 # COOLDOWN TIME IN SECONDS TO PREVENT RAPID ZOOM CHANGES
 
-while capture.isOpened():
-    ret, frame = capture.read()
+zoom_state = "STABLE" 
+
+# STEP ZOOM BEHAVIOUR
+zoom_display_time = 0
+ZOOM_DISPLAY_DURATION = 0.25 # ZOOM STATE DISPLAY DURATION IN SECONDS
+
+capture = cv2.VideoCapture(0) # TAKE INPUT FROM WEBCAM
+
+import pyautogui
+pyautogui.FAILSAFE = False
+
+while capture.isOpened(): 
+    ret, frame = capture.read() # READ VIDEO FRAME BY FRAME
     if not ret:
         print("Failed to capture video.")
         break
     frame = cv2.flip(frame, 1)
     h, w, _ = frame.shape
     
+    # CONVERT BGR TO RGB AND CREATE MEDIAPIPE IMAGE
     mp_image = mp.Image(
         image_format=mp.ImageFormat.SRGB,
         data=cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     )
     
+    # DETECT HAND LANDMARKS
     timestamp = int(time.time() * 1000)
     result = landmarks.detect_for_video(mp_image, timestamp)
     
+    # SMOOTH LANDMARKS LOOP
     if result.hand_landmarks:
         hand = result.hand_landmarks[0]
         
         if smoothed_landmarks is None:
-            smoothed_landmarks = []
+            smoothed_landmarks = [] # INITIALIZE SMOOTHED LANDMARKS FOR EACH FINGER
             for lm in hand:
                 smoothed_landmarks.append([lm.x, lm.y, lm.z])
         else:
             for i in range(21):
+                # USE EMA FOR SMOOTHING LANDMARKS
                 smoothed_landmarks[i][0] = ALPHA * hand[i].x + (1 - ALPHA) * smoothed_landmarks[i][0]
                 smoothed_landmarks[i][1] = ALPHA * hand[i].y + (1 - ALPHA) * smoothed_landmarks[i][1]
                 smoothed_landmarks[i][2] = ALPHA * hand[i].z + (1 - ALPHA) * smoothed_landmarks[i][2]
-                
+        
+        # DRAW LANDMARKS ON THE FRAME
         for lm in smoothed_landmarks:
             x, y = int(lm[0] * w), int(lm[1] * h)
             cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
@@ -62,27 +80,27 @@ while capture.isOpened():
         thumb_tip = smoothed_landmarks[4]
         thumb_pip = smoothed_landmarks[3]
         thumb_mcp = smoothed_landmarks[2]
-        thumb_up = thumb_tip[1] < thumb_pip[1] < thumb_mcp[1]
+        thumb_up = thumb_tip[1] < thumb_pip[1] < thumb_mcp[1] # CHECK IS THUMB FINGER IS UP OR NOT
                   
         index_tip = smoothed_landmarks[8]
         index_pip = smoothed_landmarks[6]
         index_mcp = smoothed_landmarks[5]
-        index_up = index_tip[1] < index_pip[1] < index_mcp[1]
+        index_up = index_tip[1] < index_pip[1] < index_mcp[1] # CHECK IS INDEX FINGER IS UP OR NOT
         
         middle_tip = smoothed_landmarks[12]
         middle_pip = smoothed_landmarks[10]
         middle_mcp = smoothed_landmarks[9]
-        middle_up = middle_tip[1] < middle_pip[1] < middle_mcp[1]
+        middle_up = middle_tip[1] < middle_pip[1] < middle_mcp[1] # CHECK IS MIDDLE FINGER IS UP OR NOT
         
         ringfinger_tip = smoothed_landmarks[16]
         ringfinger_pip = smoothed_landmarks[14]
         ringfinger_mcp = smoothed_landmarks[13]
-        ringfinger_up = ringfinger_tip[1] < ringfinger_pip[1]
+        ringfinger_up = ringfinger_tip[1] < ringfinger_pip[1] < ringfinger_mcp[1] # CHECK IS RING FINGER IS UP OR NOT
         
         pinky_tip = smoothed_landmarks[20]
         pinky_pip = smoothed_landmarks[18]
         pinky_mcp = smoothed_landmarks[17]
-        pinky_up = pinky_tip[1] < pinky_pip[1] < pinky_mcp[1]
+        pinky_up = pinky_tip[1] < pinky_pip[1] < pinky_mcp[1] # CHECK IS PINKY IS UP OR NOT
         
         # COMPUTE EUCLIDEAN DISTANCE FOR ZOOM GESTURE
         dx = thumb_tip[0] - index_tip[0]
@@ -97,13 +115,35 @@ while capture.isOpened():
         if prev_distance_smooth is None:
             prev_distance_smooth = distance
             delta = 0
-        else:
-            distance_smooth = DIST_ALPHA * distance + (1 - DIST_ALPHA) * prev_distance_smooth
+        else: 
+            distance_smooth = 0.4 * distance + 0.6 * prev_distance_smooth
             delta = distance_smooth - prev_distance_smooth
             prev_distance_smooth = distance_smooth
         
+        current_time = time.time()
+        
+        if MIN_CONTROL < distance < MAX_CONTROL and \
+        finger_state_array[0] and finger_state_array[1] and not any(finger_state_array[2:]):
+            
+            if abs(delta) > DELTA_THRESHOLD and (current_time - last_zoom_time) > ZOOM_COOLDOWN:
+                if delta > 0:
+                    zoom_state = "ZOOM OUT"
+                    pyautogui.hotkey('ctrl', '-')
+                else:
+                    zoom_state = "ZOOM IN"
+                    pyautogui.hotkey('ctrl', '+')
+                    
+                last_zoom_time = current_time
+                zoom_display_time = current_time
+        
+        if current_time - zoom_display_time > ZOOM_DISPLAY_DURATION:
+            zoom_state = "STABLE"
+
+        cv2.putText(frame, zoom_state, (10,110),
+            cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,0), 2)
+        
         if finger_state_array[0] and finger_state_array[1] and not any(finger_state_array[2:]):
-            cv2.putText(frame, f"Distance: {distance:.2f}, Delta: {delta:.2f}", (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
+            cv2.putText(frame, f"Distance: {distance:.3f}, Delta: {delta:.4f}", (10, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,255,0), 2)
             
         
     # FPS calculation
